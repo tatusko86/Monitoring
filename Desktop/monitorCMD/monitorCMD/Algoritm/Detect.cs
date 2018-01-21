26ac7bd8c36f364087e473b4fc22e61a083ccd5c
tatusko86 <tatusko86@gmail.com>
2018-01-21 20:06:38 +0200

    first commit

---
+++ Desktop/monitorCMD/monitorCMD/Algoritm/Detect.cs
@@ -0,0 +1,235 @@
+﻿using monitorCMD.Regedits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monitorCMD
+{
+    class Detect
+    {
+       
+
+        string resultToEmail;
+        public void mechingConterparty(Counterparty _new, Counterparty old)
+        {
+            
+            bool flagNewLoadInfo = false;
+
+            if (_new.fullName.Length != old.fullName.Length)
+            {
+                flagNewLoadInfo = true;
+
+                
+
+                resultToEmail = "\r\n\r\nВідбулася зміна назви:------> Була назва: " + old.fullName+"-------> Нова назва: "+_new.fullName;
+            }
+
+            if (_new.smallName != old.smallName)
+            {
+                flagNewLoadInfo = true;
+
+                
+
+                resultToEmail += "\r\n\r\nВідбулася зміна назви:------> Була назва: " + old.smallName + "-------> Нова назва: " + _new.smallName;
+            }
+
+            if (_new.address != old.address)
+            {
+                flagNewLoadInfo = true;
+
+               
+
+                resultToEmail += "\r\n\r\nВідбулася зміна адреси:------> Була адреса: " + old.address + "-------> Нова адреса: " + _new.address;
+            }
+
+            if (_new.qved != old.qved)
+            {
+                flagNewLoadInfo = true;
+
+                
+
+                resultToEmail += "\r\n\r\nВідбулася зміна основного КВЕД:------> Був квед: " + old.qved + "-------> Новий квед: " + _new.qved;
+            }
+
+            if (_new.director != old.director)
+            {
+                flagNewLoadInfo = true;
+
+                resultToEmail += "\r\n\r\nВідбулася зміна директора:------> Колишній директор: " + old.director + "-------> Новий директор: " + _new.director;
+            }
+            if (_new.status != old.status)
+            {
+                flagNewLoadInfo = true;
+
+                resultToEmail += "\r\n\r\nВідбулася зміна стану:------> Був стан: " + old.status + "-------> Новый стан: " + _new.status;
+            }
+            //-----------------------------
+            if (_new.podatBorg != old.podatBorg)
+            {
+                if (old.podatBorg == "")
+                {
+                    resultToEmail += "\r\n\r\n Наявна інформація щодо Податкового боргу - " + _new.podatBorg;
+                }
+                else
+                {
+                    resultToEmail += "\r\n\r\nВідбулася зміна стану подоткового боргу:------> Був борг: " + old.podatBorg + "-------> Став борг: " + _new.podatBorg;
+                }
+                flagNewLoadInfo = true;
+            }
+
+
+            string[] massPDVnew = _new.pdv.Split(',');
+            string[] massPDVold = old.pdv.Split(',');
+
+            if (massPDVnew[0]!= massPDVold[0])
+            {
+                if (old.pdv=="")
+                {
+                    resultToEmail += "\r\n\r\n Наявна інформація щодо реестрації свідоцтва ПДВ - " + _new.pdv;
+                }
+                else
+                {
+                    resultToEmail += "\r\n\r\nВідбулася зміна стану реестрації свідоцтва ПДВ:------> Був : " + old.pdv + "-------> Став : " + _new.pdv;
+                }
+                flagNewLoadInfo = true;
+            }
+
+            if (_new.founder.Count != 0||old.founder.Count != 0)
+            {
+                if (_new.founder.Count != 0 & old.founder.Count == 0)
+                {
+                    string fonder = "Нові засновники:\r\n\r\n";
+                    foreach (Founder s in _new.founder)
+                    {
+
+                        fonder += "\r\n" + s.name + " " + s.status + " " + s.summ + " " + s.chast;
+
+                    }
+                    resultToEmail += "\r\n\r\n" + fonder;
+                    flagNewLoadInfo = true;
+                }
+                if (_new.founder.Count == 0 & old.founder.Count != 0)
+                {
+                    
+
+                    
+                }
+                if (_new.founder.Count != 0 & old.founder.Count != 0)
+                {
+                    Validator val = new Validator();
+                    string newFounders = "";
+                    string oldFounders = "";
+
+                    List<Founder> newFolder = val.getValidFouders(_new , old);
+                    List<Founder> oldFounder = val.getValidFouders(old, _new);
+
+                    if (newFolder.Count!=0)
+                    {
+                        string fonder = "\r\n\r\nНові засновники:";
+                        foreach (Founder s in newFolder)
+                        {
+
+                            fonder += "\r\n" + s.name + " " + s.status + " " + s.summ + " " + s.chast;
+
+                        }
+                        resultToEmail += "\r\n\r\n" + fonder;
+                        flagNewLoadInfo = true;
+                    }
+
+                    if (oldFounder.Count!=0)
+                    {
+                        string fonder = "\r\n\r\nЗасновники які вишли зі складу:";
+                        foreach (Founder s in oldFounder)
+                        {
+
+                            fonder += "\r\n" + s.name + " " + s.status + " " + s.summ + " " + s.chast;
+
+                        }
+                        resultToEmail += "\r\n\r\n" + fonder;
+                        flagNewLoadInfo = true;
+                    }
+
+                        //flagNewLoadInfo = true;
+                }
+            }
+
+            if (_new.sud != old.sud)
+            {
+                
+                string result = "\r\n\r\nНові судові рішення:\r\n\r\n" + bildSud(_new.agent, old.sud, old.edrpou);
+                resultToEmail += "\r\n\r\n" + result;
+                flagNewLoadInfo = true;
+            }
+
+
+            //-----------------------------
+            if (flagNewLoadInfo == false)
+            {
+                DBconnector db = new DBconnector();
+                db.setUpdateDataOpendata(_new.edrpou); // Обновляем дату обновления в таблице ОпенДата
+                db.setUpdateDateEdrpou(_new.edrpou);   // Обновляем дату обновления в таблице Контрагент
+            }
+            else
+            {
+                DBconnector db = new DBconnector();
+                Mail mail = new Mail();
+
+                string[] mass = db.getMails(_new.edrpou).ToArray();// получаем все имейлы клиентов которые указали мониторинг по данному предприятию
+
+                foreach(string s in mass)// отправляем имейлы
+                {
+                    mail.getMail(s,_new.smallName,resultToEmail, null, "counterparty.info@gmail.com", "Grom5657184");
+                }
+
+                db.setInformationsFromOpenDataAndFounders(_new); // подливкa новых данных в таблицу ОПЕНДАТА
+
+                db.setUpdateDateEdrpou(_new.edrpou);// Обновляем дату обновления в таблице Контрагент
+                //-------------------------------------------
+            }
+
+        }
+        private string bildSud(Agent[] inBlank, DateTime data,string okpo)
+        {
+            Agent[] agent;
+            try
+            {
+                agent = inBlank;
+            }
+            catch
+            {
+                agent = new Agent[1];
+                return "Єдиний державний реєстр судових рішень тимчасово недучтупний, повторіть запит через декілька хвилин";
+            }
+
+            string result;
+            if (agent[0].Blank == null)
+            { result = "Судові рішення відсутні"; }
+            else
+            {
+                result = "⚖️\r\nСтаном на <b>" + DateTime.Now + "</b> у судовому реєстрі за запитом <b>" + okpo + "</b> знайдено <b>" + agent[0].Blank.Length + "</b> документів \r\n\r\n\r\n";
+                for (int i = 0; i < agent[0].Blank.Length; i++)
+                {
+                    string dd = data.ToString("dd.MM.yyyy");
+                    if (dd == agent[0].Blank[i].Data) { break; }
+                    try
+                    {
+                        result += "<b>" + agent[0].Blank[i].sud + "</b>\r\n" +
+                             "Дата: " + agent[0].Blank[i].Data + "\r\n" +
+                             "Тип: " + agent[0].Blank[i].TypeRisheniya + "\r\n" +
+                             "Справа: " + agent[0].Blank[i].Number + "\r\n" +
+                             "Форма: " + agent[0].Blank[i].Type + "\r\n" +
+                             "<a href=\"http://www.reyestr.court.gov.ua" + agent[0].Blank[i].Href + "\">Відкрити " + agent[0].Blank[i].TypeRisheniya.Replace("Ухвала", "Ухвалу") + "</a>\r\n\r\n\r\n";
+                    }
+                    catch { break; }
+
+                }
+
+                result += "<a href=\"http://www.reyestr.court.gov.ua\">Переглянути всі записи за даним підприємством</a>";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Desktop/monitorCMD/monitorCMD/Parsers/Counterparty.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using monitorCMD.Regedits;
+
+namespace monitorCMD
+{
+    class Counterparty
+    {
+        public Counterparty(string valueHtml,string edrpou)
+        {
+            this.valueHtml = valueHtml;
+            this.edrpou = edrpou;
+        }
+        public int id { get; set; }
+        public string fullName { get; set; }
+        public string smallName { get; set; }
+        public string edrpou { get; set; }
+        public string address { get; set; }
+        public string director { get; set; }
+        public string status { get; set; }
+        public string qved { get; set; }
+        public string contact { get; set; }
+
+        //-----------------------------
+        public List<Founder>founder = new List<Founder>();
+        //---------------------------------------
+        public string pdv { get; set; }
+        public DateTime sud { get; set; }
+        public string planPerevirki { get; set; }
+        public string podatBorg { get; set; }
+        public int viconavchiProvadgenya { get; set; }
+        //---------------------------------------------
+        public DateTime dalaLoad { get; set; }
+        public DateTime dataUpdate { get; set; }
+        string valueHtml = null;
+        public bool flag;
+        private string parser(string pathStart,string pathFin)
+        {
+            int indexStart = valueHtml.IndexOf(pathStart);
+            if (indexStart == -1) { return ""; }
+            int indexFinish = valueHtml.IndexOf(pathFin, indexStart);
+            
+            string result = valueHtml.Substring(indexStart,indexFinish-indexStart);
+            result = result.Replace(pathStart, "");
+            return result;
+        }
+
+        private void parsFounder()
+        {
+            string result = string.Empty;
+            int indexStart = valueHtml.IndexOf("Власники");
+            if (indexStart != -1)
+            {
+                int indexFinish = valueHtml.IndexOf("</h3>", indexStart);
+                int indexlast = valueHtml.IndexOf("<h3>", indexFinish);
+                result = valueHtml.Substring(indexFinish, indexlast - indexFinish);
+                result = result.Replace("\n", "");
+                result = result.Replace("<p>", " ");
+                result = result.Replace("</b>", "");
+                result = result.Replace("<b>", "");
+                result = result.Replace("</h3>", " ");
+                result = result.Replace("</p>", "*");
+                result = result.Replace("     ", " ");
+                result = result.Replace("    ", " ");
+                result = result.Replace("   ", " ");
+                result = result.Replace("  ", " ");
+                result = result.Replace("  ", " ");
+                string[] mass = result.Split('*');
+                mass = mass.Where(x => x != "").ToArray();
+                mass = mass.Where(x => x != " ").ToArray();
+                foreach (string s in mass)
+                {
+                    int index = s.IndexOf("БЕНЕФ");
+                    if (index == -1)
+                    {
+                        string[] m = s.Split(',');
+                        Founder f = new Founder();
+                        if (m.Length == 2)
+                        {
+                            f.name = m[0];
+                            f.summ = m[1];
+                            f.chast = "";
+                            f.status = "Засновник";
+                        }
+                        else if (m.Length == 1)
+                        {
+                            f.name = m[0];
+                            f.summ = "";
+                            f.chast = "";
+                            f.status = "Засновник";
+                        }
+                        else
+                        {
+                            f.name = m[0];
+                            f.summ = m[1];
+                            f.chast = m[2];
+                            f.status = "Засновник";
+                            founder.Add(f);
+                        }
+
+                    }
+                    else
+                    {
+                        Founder f = new Founder();
+                        f.status = "Бенефіціар";
+                        f.name = s;
+                        f.chast = "";
+                        f.summ = "";
+                        founder.Add(f);
+                    }
+                }
+            }
+           
+
+        }
+        private string parsBorgPDV(string start)
+        {
+            string result = string.Empty;
+            int indexStart = valueHtml.IndexOf(start);
+            if (indexStart == -1) { return ""; }
+            int indexFinish = valueHtml.IndexOf("<p>", indexStart);
+            int indexlast = valueHtml.IndexOf("</p>", indexFinish);
+            result = valueHtml.Substring(indexFinish, indexlast - indexFinish);
+            result = result.Replace("\n", "");
+            result = result.Replace("<p>", " ");
+            result = result.Replace("</b>", "");
+            result = result.Replace("<b>", "");
+            result = result.Replace("</h3>", " ");
+            result = result.Replace("</p>", "*");
+            result = result.Replace("     ", " ");
+            result = result.Replace("    ", " ");
+            result = result.Replace("   ", " ");
+            result = result.Replace("  ", " ");
+            result = result.Replace("  ", " ");
+            return result;
+            
+        }
+        public Counterparty chekInfo()
+        {
+            Counterparty con = null;
+            fullName = parser("Повна назва:","<");
+            smallName = parser("<h2>", "</h2>");
+            address = parser("Адреса:", "<");
+            director = parser("Директор:", "<");
+            status = parser("Стан:", "<");
+            qved = parser("Вид діяльністі:", "<");
+            parsFounder();
+            podatBorg = parsBorgPDV("<h3>Податковий борг</h3>"); 
+            pdv = parsBorgPDV("<h3>Платник ПДВ</h3>");
+
+            InBlank inblank = new InBlank();
+            sud = inblank.getLostDate(edrpou);
+            //sud = inblank.getCount(edrpou); // заполняем поля информацией с реестра судебных решений(количество судебных решений)/// Сейчас берем по количеству записей в реестре судебных решений
+            //а надо сделать чтобы по дате!!! Пишем дату в БД и проверяем потом
+
+            DBconnector dbcon = new DBconnector(); // add new info from OPENDATA
+             Counterparty counterparty = new Counterparty("","");
+
+            counterparty.fullName = fullName;
+            counterparty.smallName = smallName;
+            counterparty.address = address;
+            counterparty.qved = qved;
+            counterparty.director = director;
+            counterparty.status = status;
+            counterparty.pdv = pdv;
+            counterparty.podatBorg = podatBorg;
+            counterparty.sud = sud;
+            counterparty.edrpou = edrpou;
+            counterparty.founder = founder;
+
+            flag = dbcon.setNewInformationsFromOpenData(counterparty);
+
+            if (!flag)
+            {
+             con = getConterpartyInDB(edrpou);
+            }
+
+            return con;
+        }
+        private Counterparty getConterpartyInDB(string edrpou)
+        {
+            
+            Counterparty con;
+
+            DBconnector db = new DBconnector();
+            con = db.getInfoOpendata(edrpou);
+
+            return con;
+
+        }
+    }
+
+    
+}
